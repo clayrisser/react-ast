bc2c2311156427cfd6a7f8e2d7b925e7b6364bfe
Clay Risser <email@clayrisser.com>
2021-01-30 18:54:13 -0600

    Completed phase F

--- src/reconciler.ts
+++ src/reconciler.ts
@@ -1,5 +1,5 @@
 import ReactReconciler from 'react-reconciler';
-import createElement from './createElement';
+import createElement from '~/createElement';
 import {
   ChildSet,
   Container,
@@ -13,10 +13,9 @@ import {
   TimeoutHandle,
   Type,
   UpdatePayload
-} from './types';
+} from '~/types';
 
-// eslint-disable-next-line no-console
-const log = console;
+const logger = console;
 
 // bindings to the react reconciliation lifecycle methods
 export default ReactReconciler<
@@ -39,15 +38,16 @@ export default ReactReconciler<
     _rootContainerInstance: Container,
     _hostContext: HostContext
   ): Instance {
-    log.debug('createInstance');
+    logger.debug('createInstance');
     return createElement(type, props);
   },
 
   appendInitialChild(
-    _parentInstance: Instance,
-    _child: Instance | TextInstance
+    parentInstance: Instance,
+    child: Instance | TextInstance
   ): void {
-    log.debug('appendInitialChild');
+    logger.debug('appendInitialChild');
+    parentInstance.appendChild(child);
   },
 
   finalizeInitialChildren(
@@ -57,7 +57,7 @@ export default ReactReconciler<
     _rootContainerInstance: Container,
     _hostContext: HostContext
   ): boolean {
-    log.debug('finalizeInitialChildren');
+    logger.debug('finalizeInitialChildren');
     return false;
   },
 
@@ -67,16 +67,16 @@ export default ReactReconciler<
     _hostContext: HostContext
     // @ts-ignore
   ): TextInstance {
-    log.debug('createTextInstance');
+    logger.debug('createTextInstance');
   },
 
   getPublicInstance(instance: Instance | TextInstance): PublicInstance {
-    log.debug('getPublicInstance');
+    logger.debug('getPublicInstance');
     return instance;
   },
 
   prepareForCommit(_containerInfo: Container): void {
-    log.debug('prepareForCommit');
+    logger.debug('prepareForCommit');
   },
 
   prepareUpdate(
@@ -87,16 +87,16 @@ export default ReactReconciler<
     _rootContainerInstance: Container,
     _hostContext: HostContext
   ): null | UpdatePayload {
-    log.debug('prepareUpdate');
+    logger.debug('prepareUpdate');
     return true;
   },
 
   resetAfterCommit(_containerInfo: Container): void {
-    log.debug('resetAfterCommit');
+    logger.debug('resetAfterCommit');
   },
 
   resetTextContent(_instance: Instance): void {
-    log.debug('resetTextContent');
+    logger.debug('resetTextContent');
   },
 
   commitTextUpdate(
@@ -104,21 +104,19 @@ export default ReactReconciler<
     _oldText: string,
     _newText: string
   ): void {
-    log.debug('commitTextUpdate');
+    logger.debug('commitTextUpdate');
   },
 
-  removeChild(
-    _parentInstance: Instance,
-    _child: Instance | TextInstance
-  ): void {
-    log.debug('removeChild');
+  removeChild(parentInstance: Instance, child: Instance | TextInstance): void {
+    logger.debug('removeChild');
+    parentInstance.removeChild(child);
   },
 
   removeChildFromContainer(
     _container: Container,
     _child: Instance | TextInstance
   ): void {
-    log.debug('removeChildFromContainer');
+    logger.debug('removeChildFromContainer');
   },
 
   insertBefore(
@@ -126,32 +124,30 @@ export default ReactReconciler<
     _child: Instance | TextInstance,
     _beforeChild: Instance | TextInstance
   ): void {
-    log.debug('insertBefore');
+    logger.debug('insertBefore');
   },
 
   appendChildToContainer(
     container: Container,
     child: Instance | TextInstance
   ): void {
-    log.debug('appendChildToContainer');
+    logger.debug('appendChildToContainer');
     container.appendChild(child);
   },
 
-  appendChild(
-    _parentInstance: Instance,
-    _child: Instance | TextInstance
-  ): void {
-    log.debug('appendChild');
+  appendChild(parentInstance: Instance, child: Instance | TextInstance): void {
+    logger.debug('appendChild');
+    parentInstance.appendChild(child);
   },
 
   shouldSetTextContent(_type: Type, _props: Props): boolean {
-    log.debug('shouldSetTextContent');
+    logger.debug('shouldSetTextContent');
     return false;
   },
 
   // @ts-ignore
   getRootHostContext(_rootContainerInstance: Container): HostContext {
-    log.debug('getRootHostContext');
+    logger.debug('getRootHostContext');
   },
 
   getChildHostContext(
@@ -160,7 +156,7 @@ export default ReactReconciler<
     _rootContainerInstance: Container
     // @ts-ignore
   ): HostContext {
-    log.debug('getChildHostContext');
+    logger.debug('getChildHostContext');
   },
 
   now: Date.now,
@@ -172,15 +168,15 @@ export default ReactReconciler<
     _oldProps: Props,
     _newProps: Props
   ): void {
-    log.debug('commitUpdate');
+    logger.debug('commitUpdate');
   },
 
   commitMount(_instance: Instance, _type: Type, _newProps: Props): void {
-    log.debug('commitMount');
+    logger.debug('commitMount');
   },
 
   shouldDeprioritizeSubtree(): boolean {
-    log.debug('shouldDeprioritizeSubtree');
+    logger.debug('shouldDeprioritizeSubtree');
     return false;
   },
 
@@ -188,22 +184,22 @@ export default ReactReconciler<
     _callback?: () => any,
     _options?: { timeout: number }
   ): any {
-    log.debug('scheduleDeferredCallback');
+    logger.debug('scheduleDeferredCallback');
   },
 
   cancelDeferredCallback(_callbackID: any): void {
-    log.debug('cancelDeferredCallback');
+    logger.debug('cancelDeferredCallback');
   },
 
   setTimeout(
     _handler: (...args: any[]) => void,
     _timeout: number
   ): TimeoutHandle | NoTimeout {
-    log.debug('setTimeout');
+    logger.debug('setTimeout');
   },
 
   clearTimeout(_handle: TimeoutHandle | NoTimeout): void {
-    log.debug('clearTimeout');
+    logger.debug('clearTimeout');
   },
 
   noTimeout: -1 as NoTimeout,
