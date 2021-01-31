d867ef37b9d4b266c768b5a78956960d780c4970
Clay Risser <email@clayrisser.com>
2021-01-31 03:20:51 -0600

    Fixed small type bug

--- src/createElement.ts
+++ src/createElement.ts
@@ -1,10 +1,10 @@
 import elements from '~/elements';
-import { ElementConstructor } from '~/elements/Element';
+import { IElement } from '~/elements/BaseElement';
 import { getContext } from '~/context';
 
 export default function createElement(elementType: string, props: any) {
   const { parserOptions } = getContext();
-  const Element: ElementConstructor = elements[elementType];
+  const Element: IElement = elements[elementType];
   if (!Element) throw new Error(`unknown element of type '${elementType}'`);
   return new Element(props, parserOptions);
 }
