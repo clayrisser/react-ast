d3c54cca35c819943e3448ee4919a0ecf257b43d
Dharmendra <dharmendra.developer6@gmail.com>
2023-11-27 10:10:08 +0530

    test(ObjectLiteral): renders object and compares the code with expected output

--- src/components/literals/ObjectLiteral/index.spec.tsx
+++ src/components/literals/ObjectLiteral/index.spec.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { render, ObjectLiteral, CallExpression, Var } from '~/index';
 
-// describe('<ObjectLiteral />', () => {
-//   it('renders object', () => {
-//     const code = render(
-//       <ObjectLiteral debug>{JSON.stringify({ a: 1 })}</ObjectLiteral>,
-//       { prettier: false }
-//     );
-//     expect(code).toBe('{ "a": 1 }');
-//   });
-// });
+describe('<ObjectLiteral />', () => {
+  it('renders object', () => {
+    const code = render(
+      <ObjectLiteral debug>{JSON.stringify({ a: 1 })}</ObjectLiteral>,
+      { prettier: false }
+    );
+    expect(code).toBe('{\n  "a": 1\n}');
+  });
+
+  it('renders object in call expression', () => {
+    const code = render(
+      <CallExpression
+        name="test"
+        debug
+        arguments={[<ObjectLiteral>{JSON.stringify({ a: 1 })}</ObjectLiteral>]}
+      />,
+      { prettier: false }
+    );
+    expect(code).toBe('test({\n  "a": 1\n})');
+  });
+
+  it('renders object in variable', () => {
+    const code = render(
+      <Var name="test">
+        <ObjectLiteral>{JSON.stringify({ a: 1 })}</ObjectLiteral>
+      </Var>,
+      { prettier: false }
+    );
+    expect(code).toBe('var test = {\n  "a": 1\n};');
+  });
+});
