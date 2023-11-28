0f2cd335e53368ce5fb9ef538be5410daacbcf27
Dharmendra <dharmendra.developer6@gmail.com>
2023-11-28 15:06:58 +0530

    update imports in elements

--- src/elements/Ast.ts
+++ src/elements/Ast.ts
@@ -1,5 +1,26 @@
+/*
+ *  File: /src/elements/Ast.ts
+ *  Project: react-ast
+ *  File Created: 28-11-2023 15:04:04
+ *  Author: dharmendra
+ *  -----
+ *  BitSpur (c) Copyright 2019 - 2023
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 import _get from "lodash.get";
-import { flattenPath } from "~/util";
+import { flattenPath } from "../util";
 import BaseElement from "./BaseElement";
 
 export default class Ast extends BaseElement {
@@ -19,7 +40,7 @@ export default class Ast extends BaseElement {
       {
         bodyPath: props.bodyPath || "body",
         parentBodyPath: props.parentBodyPath,
-      },
+      }
     );
   }
 }
