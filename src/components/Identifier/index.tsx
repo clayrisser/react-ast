aa6f59e4a1084b89b9f8c32526c5824a596d2a03
Dharmendra <dharmendra.developer6@gmail.com>
2023-11-28 15:26:05 +0530

    update imports in identifier and interface

--- src/components/Identifier/index.tsx
+++ src/components/Identifier/index.tsx
@@ -1,10 +1,31 @@
+/**
+ * File: /src/components/Identifier/index.tsx
+ * Project: react-ast
+ * File Created: 28-11-2023 15:04:04
+ * Author: dharmendra
+ * -----
+ * BitSpur (c) Copyright 2019 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React, { Ref, ReactNode, forwardRef } from "react";
 import useMergedRef from "@react-hook/merged-ref";
-import TypeAnnotation from "~/components/types/TypeAnnotation";
-import BaseElement from "~/elements/BaseElement";
-import ParentBodyPathProvider from "~/providers/ParentBodyPathProvider";
-import Smart from "~/components/Smart";
-import { debugRef } from "~/util";
+import TypeAnnotation from "../../components/types/TypeAnnotation";
+import BaseElement from "../../elements/BaseElement";
+import ParentBodyPathProvider from "../../providers/ParentBodyPathProvider";
+import Smart from "../../components/Smart";
+import { debugRef } from "../../util";
 
 export interface IdentifierProps {
   children: string;
@@ -40,7 +61,7 @@ const Identifier = forwardRef<BaseElement, IdentifierProps>(
         </ParentBodyPathProvider>
       </Smart>
     );
-  },
+  }
 );
 
 Identifier.defaultProps = { debug: false };
