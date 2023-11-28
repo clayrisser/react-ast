796bbc5086c7b6f5510291b298eeecdc5119ede5
Lalit Rajak <lalitrajak312@gmail.com>
2023-11-28 15:29:25 +0530

    updated the imports

--- src/components/react/FunctionalComponent/index.tsx
+++ src/components/react/FunctionalComponent/index.tsx
@@ -1,4 +1,26 @@
-import React, { FC, ReactNode } from "react";
+/**
+ * File: /src/components/react/FunctionalComponent/index.tsx
+ * Project: react-ast
+ * File Created: 28-11-2023 15:05:44
+ * Author: Lalit rajak
+ * -----
+ * BitSpur (c) Copyright 2019 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { FC, ReactNode } from "react";
+import React from "react";
 import {
   Export,
   Expression,
@@ -12,7 +34,7 @@ import {
   TypeReference,
   Var,
   VarKind,
-} from "~/components";
+} from "../..";
 
 export interface FunctionalComponentProps {
   children?: ReactNode;
