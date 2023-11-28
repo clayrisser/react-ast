e2ea3a94bd090860380cd2911019528450103160
Clay Risser <clayrisser@gmail.com>
2023-11-27 23:55:36 -0600

    Fixed more imports

--- src/components/modules/ExportAllDeclaration/index.tsx
+++ src/components/modules/ExportAllDeclaration/index.tsx
@@ -1,8 +1,30 @@
-import React, { Ref, forwardRef } from "react";
+/**
+ * File: /src/components/modules/ExportAllDeclaration/index.tsx
+ * Project: react-ast
+ * File Created: 28-11-2023 02:58:22
+ * Author: Clay Risser
+ * -----
+ * BitSpur (c) Copyright 2019 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { forwardRef } from "react";
+import Smart from "../../../components/Smart";
+import type BaseElement from "../../../elements/BaseElement";
+import type { Ref } from "react";
 import useMergedRef from "@react-hook/merged-ref";
-import BaseElement from "~/elements/BaseElement";
-import Smart from "~/components/Smart";
-import { debugRef } from "~/util";
+import { debugRef } from "../../../util";
 
 export interface ExportAllDeclarationProps {
   debug?: boolean;
