a73546b31b3a605cd488e7fda5c015d4fd09a687
Phani Bhushan <kotharuphani@gmail.com>
2023-12-08 12:35:25 +0530

    ✨ feat: Add test for FunctionDeclaration with Await and Call Expressions
    This commit adds a new test case for the `FunctionDeclaration` component with an await expression and a call expression. The test verifies that the rendered code matches the expected result.
    
    The test creates a function declaration with the identifier "getData" and a parameter "url". Inside the function, it declares a constant variable "result" and assigns it the value of an await expression containing a call expression to the "fetch" function with the "url" argument.
    
    The test uses the `render` function from the `react-ast` package to generate the code and asserts that the generated code matches the expected code.
    
    This test ensures that the `FunctionDeclaration` component correctly handles await expressions and call expressions within its body.

---
+++ src/components/expressions/AwaitExpression/index.spec.tsx
@@ -0,0 +1,76 @@
+/**
+ * File: /src/components/expressions/AwaitExpression/index.spec.tsx
+ * Project: react-ast
+ * File Created: 08-12-2023 12:34:57
+ * Author: K S R PHANI BHUSHAN
+ * -----
+ * BitSpur (c) Copyright 2019 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * File: /src/components/expressions/AwaitExpression/index.spec.tsx
+ * Project: react-ast
+ * File Created: 08-12-2023 12:07:34
+ * Author: K S R PHANI BHUSHAN
+ * -----
+ * BitSpur (c) Copyright 2019 - 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from "react";
+import { render } from "../../../index";
+import FunctionDeclaration from "../../../components/functions/FunctionDeclaration";
+import VariableDeclaration, {
+  VariableDeclarationKind,
+} from "../../../components/variables/VariableDeclaration";
+import VariableDeclarator from "../../../components/variables/VariableDeclarator";
+import AwaitExpression from "../../../components/expressions/AwaitExpression";
+import CallExpression from "../../../components/expressions/CallExpression";
+import Identifier from "../../../components/Identifier";
+
+describe("FunctionDeclaration with Await and Call Expressions", () => {
+  it("renders a function declaration with an await expression and a call expression", async () => {
+    const code = await render(
+      <FunctionDeclaration async id="getData" params={["url"]}>
+        <VariableDeclaration kind={VariableDeclarationKind.Const}>
+          <VariableDeclarator id="result">
+            <AwaitExpression>
+              <CallExpression
+                name="fetch"
+                arguments={<Identifier>url</Identifier>}
+              />
+            </AwaitExpression>
+          </VariableDeclarator>
+        </VariableDeclaration>
+      </FunctionDeclaration>,
+      {
+        prettier: false,
+      },
+    );
+    expect(code).toBe(`async function getData(url) {
+  const result = await fetch(url);
+}`);
+  });
+});
